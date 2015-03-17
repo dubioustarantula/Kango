0d92402ff9d1d04555a1aea0b9ccabf68a29e673
lpww <t.treffry@gmail.com>
2015-03-16 21:46:13 -0700

    (feature)Add donation routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,12 @@ var indexRouter = express.Router();
 indexRouter.route('/')
   .all(controllers.index);
 
-indexRouter.get('/shelters', controllers.getShelters);
 indexRouter.post('/shelter', controllers.postShelter);
+indexRouter.get('/shelters', controllers.getShelters);
 
 indexRouter.get('/users', controllers.getUsers);
+
+indexRouter.post('/donation', controllers.postDonation);
+indexRouter.get('/donation', controllers.getDonations);
 
 exports.indexRouter = indexRouter;
