3b9faaaed2b22a4b04aa59a1b978e8fb97625b14
Julia Nething <cjnething@gmail.com>
2015-03-18 09:22:24 -0700

    (feat) Set up passport.js to serialize user and allow user to sign up

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var Donation = require('./donation');
 var bookshelf = require('../config');
+var bcrypt = require('bcrypt-nodejs');
 
 var User = bookshelf.Model.extend({
   
@@ -13,3 +14,36 @@ var User = bookshelf.Model.extend({
 });
 
 module.exports = User;
+
+
+
+
+// define the schema for our user model
+// var userSchema = mongoose.Schema({
+
+//     local            : {
+//         email        : String,
+//         password     : String,
+//     },
+//     twitter          : {
+//         id           : String,
+//         token        : String,
+//         displayName  : String,
+//         username     : String
+//     }
+
+// });
+
+// methods ======================
+// generating a hash
+// userSchema.methods.generateHash = function(password) {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// };
+
+// checking if password is valid
+// userSchema.methods.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.local.password);
+// };
+
+// create the model for users and expose it to our app
+// module.exports = mongoose.model('User', userSchema);
