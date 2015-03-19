99a793421b515dfaa5d87c2873b4d2c020482762
Julia Nething <cjnething@gmail.com>
2015-03-19 16:23:25 -0700

    (refactor) Untrack keys/auth.js file

--- keys/auth.js
+++
@@ -1,9 +0,0 @@
-// expose our config directly to our application using module.exports
-module.exports = {
-
-    'twitterAuth' : {
-        'consumerKey'       : '9T6wNoiCgdZuO9wtLMhun7LPl',
-        'consumerSecret'    : 'yzKOCTz6mjVV8SZUvKLW0kAlRe4d2dU6W99WPahUxJzyYY5AM6',
-        'callbackURL'       : 'http://www.kango.com/profile'
-    }
-};
